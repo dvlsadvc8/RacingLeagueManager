350041c6850a86dd39603e42803d1c8031c37158
Brad <brad.norris@live.com>
2018-10-12 01:37:17 -0500

    Certify results added. Points calculated.

--- RacingLeagueManager/Pages/Race/Details.cshtml.cs
+++ RacingLeagueManager/Pages/Race/Details.cshtml.cs
@@ -30,15 +30,95 @@ namespace RacingLeagueManager.Pages.Race
                 return NotFound();
             }
 
-            var Race = await _context.Race
-                //.Include(r => r.Results)
+            var Race = await GetRace(id.Value);
+
+            if (Race == null)
+            {
+                return NotFound();
+            }
+            
+            RaceDetailsViewModel = GetViewModel(Race, GetResultsList(Race));
+            
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostStatusAsync(Guid raceId)
+        {
+            if(raceId == null)
+            {
+                return NotFound();
+            }
+
+            var race = await _context.Race.FirstOrDefaultAsync(r => r.Id == raceId);
+
+            if(race == null)
+            {
+                return NotFound();
+            }
+
+            if(race.Status == RaceStatus.Closed)
+            {
+                try
+                {
+                    await CertifyRace(race.Id);
+                }
+                catch(ArgumentNullException ex)
+                {
+                    return NotFound();
+                }
+            }
+
+            race.Status += 1;
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("Details", new { id = raceId });
+        }
+
+        private async Task CertifyRace(Guid raceId)
+        {
+            if (raceId == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            var race = await GetRace(raceId);
+
+            if(race == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            RaceDetailsViewModel model = GetViewModel(race, GetResultsList(race));
+
+            List<Data.Models.RaceResult> raceResultList = await _context.RaceResult.Where(r => r.RaceId == race.Id).ToListAsync();
+
+
+            foreach(var resultModel in model.Results)
+            {
+                var raceResult = raceResultList.FirstOrDefault(r => r.Id == resultModel.RaceResultId);
+
+                if(raceResult.Place == 0)
+                {
+                    raceResult.Place = resultModel.Place;
+                }
+
+                if(raceResult.ResultType != ResultType.DNS)
+                {
+                    raceResult.Points = 25 - resultModel.Place;
+                }
+            }
+        }
+
+        private async Task<Data.Models.Race> GetRace(Guid id)
+        {
+            return await _context.Race
                 .Include(r => r.Track)
                 .Include(r => r.Series)
                     .ThenInclude(s => s.SeriesEntries)
                         .ThenInclude(s => s.SeriesEntryDrivers)
                             .ThenInclude(s => s.LeagueDriver)
                                 .ThenInclude(ld => ld.Driver)
-                
                 .Include(r => r.Series)
                     .ThenInclude(s => s.SeriesEntries)
                         .ThenInclude(e => e.RaceResults)
@@ -50,14 +130,10 @@ namespace RacingLeagueManager.Pages.Race
                     .ThenInclude(s => s.SeriesEntries)
                         .ThenInclude(s => s.Team)
                 .FirstOrDefaultAsync(m => m.Id == id);
+        }
 
-            if (Race == null)
-            {
-                return NotFound();
-            }
-
-            //Select((x, index) => new { index, x.Id, x.FirstName });
-
+        private List<RaceResultViewModel> GetResultsList(Data.Models.Race Race)
+        {
             List<RaceResultViewModel> ResultsList = new List<RaceResultViewModel>();
             foreach (var entry in Race.Series.SeriesEntries)
             {
@@ -72,24 +148,32 @@ namespace RacingLeagueManager.Pages.Race
                     //Points
                 };
 
-                if(raceResult != null)
+                if (raceResult != null)
                 {
                     raceResultViewModel.RaceResultId = raceResult.Id;
                     raceResultViewModel.Username = raceResult.Driver.UserName;
                     raceResultViewModel.BestLap = raceResult.BestLap;
                     raceResultViewModel.TotalTime = raceResult.TotalTime;
                     raceResultViewModel.PenaltySeconds = raceResult.Penalties.Sum(p => p.Seconds);
-                    raceResultViewModel.OfficialTime = raceResultViewModel.TotalTime?.Add(new TimeSpan(0,0,raceResultViewModel.PenaltySeconds));
+                    raceResultViewModel.OfficialTime = raceResultViewModel.TotalTime?.Add(new TimeSpan(0, 0, raceResultViewModel.PenaltySeconds));
                     raceResultViewModel.Place = raceResult.Place;
                     raceResultViewModel.ResultType = raceResult.ResultType;
+                    raceResultViewModel.Points = raceResult.Points;
                 }
 
                 ResultsList.Add(raceResultViewModel);
             }
 
+            return ResultsList;
+        }
+
+        private RaceDetailsViewModel GetViewModel(Data.Models.Race Race, List<RaceResultViewModel> ResultsList)
+        {
+            RaceDetailsViewModel model;
+
             if (Race.Status != RaceStatus.Certified)
             {
-                RaceDetailsViewModel = new RaceDetailsViewModel()
+                model = new RaceDetailsViewModel()
                 {
                     RaceId = Race.Id,
                     TrackName = Race.Track.Name,
@@ -117,14 +201,14 @@ namespace RacingLeagueManager.Pages.Race
                         }).ToList()
                 };
 
-                RaceDetailsViewModel.Results.AddRange(
+                model.Results.AddRange(
                     ResultsList
                         .Where(r => r.TotalTime == null)
                         .OrderBy(r => r.Place)
                         .Select((x, index) =>
                             new RaceResultViewModel()
                             {
-                                Place = RaceDetailsViewModel.Results.Count + 1,
+                                Place = model.Results.Count + 1,
                                 BestLap = x.BestLap,
                                 CarName = x.CarName,
                                 OfficialTime = x.OfficialTime,
@@ -143,7 +227,7 @@ namespace RacingLeagueManager.Pages.Race
             }
             else
             {
-                RaceDetailsViewModel = new RaceDetailsViewModel()
+                model = new RaceDetailsViewModel()
                 {
                     RaceId = Race.Id,
                     TrackName = Race.Track.Name,
@@ -171,30 +255,8 @@ namespace RacingLeagueManager.Pages.Race
                         }).ToList()
                 };
             }
-            
 
-            return Page();
-        }
-
-        public async Task<IActionResult> OnPostStatusAsync(Guid raceId)
-        {
-            if(raceId == null)
-            {
-                return NotFound();
-            }
-
-            var race = await _context.Race.FirstOrDefaultAsync(r => r.Id == raceId);
-
-            if(race == null)
-            {
-                return NotFound();
-            }
-
-            race.Status += 1;
-
-            await _context.SaveChangesAsync();
-
-            return RedirectToPage("Details", new { id = raceId });
+            return model;
         }
     }
 
